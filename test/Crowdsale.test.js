b8862dcac4345a44ec16d7d75185bac7533a8e9b
HadarKeidar <37545734+HadarKeidar@users.noreply.github.com>
2018-07-22 13:16:20 +0300

    Update after audit

--- test/Crowdsale.test.js
+++ test/Crowdsale.test.js
@@ -49,14 +49,13 @@ contract('Crowdsale', ([alice, bob, charlie, damiens, owner]) => {
     token = await SpitballToken.new(database.address, {from: owner});
     crowdsale = await Crowdsale.new(owner, fundingGoal, saleDuration, etherCost, token.address, {from:owner});
     saleDurationTimestamp = latestTime() + duration.minutes(saleDuration);
-	await token.mint(crowdsale.address, fundingGoal * etherCost, {from: owner});
-	crowdsale.approve(alice, {from : owner});
-	crowdsale.approve(bob, {from : owner});
-	crowdsale.approve(charlie, {from : owner});
-	crowdsale.approve(damiens, {from : owner});
+  	await token.mint(crowdsale.address, fundingGoal * etherCost, {from: owner});
+  	await crowdsale.addAddressToWhitelist(alice, {from : owner});
+  	await crowdsale.addAddressToWhitelist(bob, {from : owner});
+  	await crowdsale.addAddressToWhitelist(charlie, {from : owner});
+  	await crowdsale.addAddressToWhitelist(damiens, {from : owner});
+    await crowdsale.addAddressToWhitelist(owner, {from: owner});
   });
-
-  
 
   describe('When considering the crowdsale has all predefined features: ', () => {
   	
