313f4d9891067ca2b3ac1f3a0bfb923e2b1e10ec
HadarKeidar <37545734+HadarKeidar@users.noreply.github.com>
2018-07-22 13:09:50 +0300

    Update after audit

--- contracts/ERC865.sol
+++ contracts/ERC865.sol
@@ -12,6 +12,15 @@ import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 
 contract ERC865 is ERC20 {
 
+    
+    /**
+    * @dev Definition of submit presigned transfer function
+    * @param _signature bytes The signature, issued by the owner.
+    * @param _to address The address which you want to transfer to.
+    * @param _value uint256 The amount of tokens to be transferred.
+    * @param _fee uint256 The amount of tokens paid to msg.sender, by the owner.
+    * @param _nonce uint256 Presigned transaction number.
+    */
     function transferPreSigned
     (
         bytes _signature,
@@ -20,10 +29,18 @@ contract ERC865 is ERC20 {
         uint256 _fee,
         uint256 _nonce
     )
-        public
+      public
         returns (bool);
 
 
+    /**
+    * @dev Definition of submit presigned approval function
+    * @param _signature bytes The signature, issued by the owner.
+    * @param _spender address The address which will spend the funds.
+    * @param _value uint256 The amount of tokens to allow.
+    * @param _fee uint256 The amount of tokens paid to msg.sender, by the owner.
+    * @param _nonce uint256 Presigned transaction number.
+    */
     function approvePreSigned
     (
         bytes _signature,
@@ -32,10 +49,18 @@ contract ERC865 is ERC20 {
         uint256 _fee,
         uint256 _nonce
     )
-        public
+      public
         returns (bool);
 
 
+    /**
+    * @dev Definition of submit presigned increase approval amount function
+    * @param _signature bytes The signature, issued by the owner.
+    * @param _spender address The address which will spend the funds.
+    * @param _addedValue uint256 The amount of tokens to increase the allowance by.
+    * @param _fee uint256 The amount of tokens paid to msg.sender, by the owner.
+    * @param _nonce uint256 Presigned transaction number.
+    */
     function increaseApprovalPreSigned
     (
         bytes _signature,
@@ -44,10 +69,18 @@ contract ERC865 is ERC20 {
         uint256 _fee,
         uint256 _nonce
     )
-        public
+      public
         returns (bool);
 
 
+    /**
+    * @dev Definition of submit presigned decrease approval amount function
+    * @param _signature bytes The signature, issued by the owner
+    * @param _spender address The address which will spend the funds.
+    * @param _subtractedValue uint256 The amount of tokens to decrease the allowance by.
+    * @param _fee uint256 The amount of tokens paid to msg.sender, by the owner.
+    * @param _nonce uint256 Presigned transaction number.
+    */
     function decreaseApprovalPreSigned
     (
         bytes _signature,
@@ -56,10 +89,19 @@ contract ERC865 is ERC20 {
         uint256 _fee,
         uint256 _nonce
     )
-        public
+      public
         returns (bool);
 
 
+    /**
+    * @dev Definition of submit presigned transfer from function
+    * @param _signature bytes The signature, issued by the spender.
+    * @param _from address The address which you want to send tokens from.
+    * @param _to address The address which you want to transfer to.
+    * @param _value uint256 The amount of tokens to be transferred.
+    * @param _fee uint256 The amount of tokens paid to msg.sender, by the spender.
+    * @param _nonce uint256 Presigned transaction number.
+    */
     function transferFromPreSigned
     (
         bytes _signature,
@@ -69,6 +111,6 @@ contract ERC865 is ERC20 {
         uint256 _fee,
         uint256 _nonce
     )
-        public
+      public
         returns (bool);
 }
