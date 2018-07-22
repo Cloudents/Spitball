2e073ebccd7aeac8374fb677510772435c4df2f7
HadarKeidar <37545734+HadarKeidar@users.noreply.github.com>
2018-07-22 13:10:42 +0300

    Update aftet audit

--- contracts/ERC865Token.sol
+++ contracts/ERC865Token.sol
@@ -15,8 +15,7 @@ import "openzeppelin-solidity/contracts/token/ERC20/MintableToken.sol";
  */
 contract ERC865Token is ERC865, PausableToken, MintableToken {
 
-    /* Nonces of transfers performed */
-    //mapping(bytes => bool) signatures;
+  /* Nonces of transfers performed */
     ERC865Database internal interfaceData;
     
     event TransferPreSigned(address indexed from, address indexed to, address indexed delegate, uint256 amount, uint256 fee);
@@ -24,33 +23,33 @@ contract ERC865Token is ERC865, PausableToken, MintableToken {
     
     constructor 
     (
-        address _interfaceData
+      address _interfaceData
     ) 
-        public
+      public
     {
         interfaceData = ERC865Database(_interfaceData);
     }
- 
+
 
     /**
-     * @notice Submit a presigned transfer
-     * @param _signature bytes The signature, issued by the owner.
-     * @param _to address The address which you want to transfer to.
-     * @param _value uint256 The amount of tokens to be transferred.
-     * @param _fee uint256 The amount of tokens paid to msg.sender, by the owner.
-     * @param _nonce uint256 Presigned transaction number.
-     */
+    * @notice Submit a presigned transfer
+    * @param _signature bytes The signature, issued by the owner.
+    * @param _to address The address which you want to transfer to.
+    * @param _value uint256 The amount of tokens to be transferred.
+    * @param _fee uint256 The amount of tokens paid to msg.sender, by the owner.
+    * @param _nonce uint256 Presigned transaction number.
+    */
     function transferPreSigned
     (
         bytes _signature,
-       // address _from,
+        // address _from,
         address _to,
         uint256 _value,
         uint256 _fee,
         uint256 _nonce
     )
-        public
-        returns (bool)
+      public
+      returns (bool)
     {
         require(_to != address(0));
         require(interfaceData.getSignatures(_signature) == false);
@@ -59,7 +58,7 @@ contract ERC865Token is ERC865, PausableToken, MintableToken {
         bytes32 hashedTx = transferPreSignedHashing(address(this), _to, _value, _fee, _nonce);
 
         address from = recover(hashedTx, _signature);
-       
+      
         require(from != address(0));
 
         balances[from] = balances[from].sub(_value).sub(_fee);
@@ -76,22 +75,23 @@ contract ERC865Token is ERC865, PausableToken, MintableToken {
 
 
     /**
-     * @notice Submit a presigned approval
-     * @param _signature bytes The signature, issued by the owner.
-     * @param _spender address The address which will spend the funds.
-     * @param _value uint256 The amount of tokens to allow.
-     * @param _fee uint256 The amount of tokens paid to msg.sender, by the owner.
-     * @param _nonce uint256 Presigned transaction number.
-     */
-    function approvePreSigned(
+    * @notice Submit a presigned approval
+    * @param _signature bytes The signature, issued by the owner.
+    * @param _spender address The address which will spend the funds.
+    * @param _value uint256 The amount of tokens to allow.
+    * @param _fee uint256 The amount of tokens paid to msg.sender, by the owner.
+    * @param _nonce uint256 Presigned transaction number.
+    */
+    function approvePreSigned
+    (
         bytes _signature,
         address _spender,
         uint256 _value,
         uint256 _fee,
         uint256 _nonce
     )
-        public
-        returns (bool)
+      public
+      returns (bool)
     {
         require(_spender != address(0));
         require(interfaceData.getSignatures(_signature) == false);
@@ -112,23 +112,25 @@ contract ERC865Token is ERC865, PausableToken, MintableToken {
         return true;
     }
 
+
     /**
-     * @notice Increase the amount of tokens that an owner allowed to a spender.
-     * @param _signature bytes The signature, issued by the owner.
-     * @param _spender address The address which will spend the funds.
-     * @param _addedValue uint256 The amount of tokens to increase the allowance by.
-     * @param _fee uint256 The amount of tokens paid to msg.sender, by the owner.
-     * @param _nonce uint256 Presigned transaction number.
-     */
-    function increaseApprovalPreSigned(
+    * @notice Increase the amount of tokens that an owner allowed to a spender.
+    * @param _signature bytes The signature, issued by the owner.
+    * @param _spender address The address which will spend the funds.
+    * @param _addedValue uint256 The amount of tokens to increase the allowance by.
+    * @param _fee uint256 The amount of tokens paid to msg.sender, by the owner.
+    * @param _nonce uint256 Presigned transaction number.
+    */
+    function increaseApprovalPreSigned
+    (
         bytes _signature,
         address _spender,
         uint256 _addedValue,
         uint256 _fee,
         uint256 _nonce
     )
-        public
-        returns (bool)
+      public
+      returns (bool)
     {
         require(_spender != address(0));
         require(interfaceData.getSignatures(_signature) == false);
@@ -150,23 +152,25 @@ contract ERC865Token is ERC865, PausableToken, MintableToken {
         return true;
     }
 
+
     /**
-     * @notice Decrease the amount of tokens that an owner allowed to a spender.
-     * @param _signature bytes The signature, issued by the owner
-     * @param _spender address The address which will spend the funds.
-     * @param _subtractedValue uint256 The amount of tokens to decrease the allowance by.
-     * @param _fee uint256 The amount of tokens paid to msg.sender, by the owner.
-     * @param _nonce uint256 Presigned transaction number.
-     */
-    function decreaseApprovalPreSigned(
+    * @notice Decrease the amount of tokens that an owner allowed to a spender.
+    * @param _signature bytes The signature, issued by the owner
+    * @param _spender address The address which will spend the funds.
+    * @param _subtractedValue uint256 The amount of tokens to decrease the allowance by.
+    * @param _fee uint256 The amount of tokens paid to msg.sender, by the owner.
+    * @param _nonce uint256 Presigned transaction number.
+    */
+    function decreaseApprovalPreSigned
+    (
         bytes _signature,
         address _spender,
         uint256 _subtractedValue,
         uint256 _fee,
         uint256 _nonce
     )
-        public
-        returns (bool)
+      public
+      returns (bool)
     {
         require(_spender != address(0));
         require(interfaceData.getSignatures(_signature) == false);
@@ -193,28 +197,28 @@ contract ERC865Token is ERC865, PausableToken, MintableToken {
         return true;
     }
 
+
     /**
-     * @notice Transfer tokens from one address to another
-     * @param _signature bytes The signature, issued by the spender.
-     * @param _from address The address which you want to send tokens from.
-     * @param _to address The address which you want to transfer to.
-     * @param _value uint256 The amount of tokens to be transferred.
-     * @param _fee uint256 The amount of tokens paid to msg.sender, by the spender.
-     * @param _nonce uint256 Presigned transaction number.
-     */
-    function transferFromPreSigned(
+    * @notice Transfer tokens from one address to another
+    * @param _signature bytes The signature, issued by the spender.
+    * @param _from address The address which you want to send tokens from.
+    * @param _to address The address which you want to transfer to.
+    * @param _value uint256 The amount of tokens to be transferred.
+    * @param _fee uint256 The amount of tokens paid to msg.sender, by the spender.
+    * @param _nonce uint256 Presigned transaction number.
+    */
+    function transferFromPreSigned
+    (
         bytes _signature,
         address _from,
         address _to,
         uint256 _value,
         uint256 _fee,
         uint256 _nonce
-
     )
-        public
-        returns (bool)
+      public
+      returns (bool)
     {
-
         require(_to != address(0));
         require(interfaceData.getSignatures(_signature) == false);
         //require(signatures[_signature] == false);
@@ -241,109 +245,118 @@ contract ERC865Token is ERC865, PausableToken, MintableToken {
 
 
     /**
-     * @notice Hash (keccak256) of the payload used by transferPreSigned
-     * @param _token address The address of the token.
-     * @param _to address The address which you want to transfer to.
-     * @param _value uint256 The amount of tokens to be transferred.
-     * @param _fee uint256 The amount of tokens paid to msg.sender, by the owner.
-     * @param _nonce uint256 Presigned transaction number.
-     */
-    function transferPreSignedHashing(
+    * @notice Hash (keccak256) of the payload used by transferPreSigned
+    * @param _token address The address of the token.
+    * @param _to address The address which you want to transfer to.
+    * @param _value uint256 The amount of tokens to be transferred.
+    * @param _fee uint256 The amount of tokens paid to msg.sender, by the owner.
+    * @param _nonce uint256 Presigned transaction number.
+    */
+    function transferPreSignedHashing
+    (
         address _token,
         address _to,
         uint256 _value,
         uint256 _fee,
         uint256 _nonce
     )
-        public
-        pure
-        returns (bytes32)
+      public
+      pure
+      returns (bytes32)
     {
-        /* "48664c16": transferPreSignedHashing(address,address,address,uint256,uint256,uint256) */
-        //bytes memory prefix = "\x19Ethereum Signed Message:\n32";
+      /* "48664c16": transferPreSignedHashing(address,address,address,uint256,uint256,uint256) */
+      //bytes memory prefix = "\x19Ethereum Signed Message:\n32";
         return keccak256(abi.encodePacked(bytes4(0x48664c16), _token, _to, _value, _fee, _nonce));
     }
 
+
     /**
-     * @notice Hash (keccak256) of the payload used by approvePreSigned
-     * @param _token address The address of the token
-     * @param _spender address The address which will spend the funds.
-     * @param _value uint256 The amount of tokens to allow.
-     * @param _fee uint256 The amount of tokens paid to msg.sender, by the owner.
-     * @param _nonce uint256 Presigned transaction number.
-     */
-    function approvePreSignedHashing(
+    * @notice Hash (keccak256) of the payload used by approvePreSigned
+    * @param _token address The address of the token
+    * @param _spender address The address which will spend the funds.
+    * @param _value uint256 The amount of tokens to allow.
+    * @param _fee uint256 The amount of tokens paid to msg.sender, by the owner.
+    * @param _nonce uint256 Presigned transaction number.
+    */
+    function approvePreSignedHashing
+    (
         address _token,
         address _spender,
         uint256 _value,
         uint256 _fee,
         uint256 _nonce
     )
-        public
-        pure
-        returns (bytes32)
+      public
+      pure
+      returns (bytes32)
     {
-        /* "f7ac9c2e": approvePreSignedHashing(address,address,uint256,uint256,uint256) */
-       // bytes memory prefix = "\x19Ethereum Signed Message:\n32";
+      /* "f7ac9c2e": approvePreSignedHashing(address,address,uint256,uint256,uint256) */
+      // bytes memory prefix = "\x19Ethereum Signed Message:\n32";
         return keccak256(abi.encodePacked(bytes4(0xf7ac9c2e), _token, _spender, _value, _fee, _nonce));
     }
 
+
     /**
-     * @notice Hash (keccak256) of the payload used by increaseApprovalPreSigned
-     * @param _token address The address of the token
-     * @param _spender address The address which will spend the funds.
-     * @param _addedValue uint256 The amount of tokens to increase the allowance by.
-     * @param _fee uint256 The amount of tokens paid to msg.sender, by the owner.
-     * @param _nonce uint256 Presigned transaction number.
-     */
-    function increaseApprovalPreSignedHashing(
+    * @notice Hash (keccak256) of the payload used by increaseApprovalPreSigned
+    * @param _token address The address of the token
+    * @param _spender address The address which will spend the funds.
+    * @param _addedValue uint256 The amount of tokens to increase the allowance by.
+    * @param _fee uint256 The amount of tokens paid to msg.sender, by the owner.
+    * @param _nonce uint256 Presigned transaction number.
+    */
+    function increaseApprovalPreSignedHashing
+    (
         address _token,
         address _spender,
         uint256 _addedValue,
         uint256 _fee,
         uint256 _nonce
     )
-        public
-        pure
-        returns (bytes32)
+      public
+      pure
+      returns (bytes32)
     {
-        /* "a45f71ff": increaseApprovalPreSignedHashing(address,address,uint256,uint256,uint256) */
+      /* "a45f71ff": increaseApprovalPreSignedHashing(address,address,uint256,uint256,uint256) */
         return keccak256(abi.encodePacked(bytes4(0xa45f71ff), _token, _spender, _addedValue, _fee, _nonce));
     }
 
-     /**
-      * @notice Hash (keccak256) of the payload used by decreaseApprovalPreSigned
-      * @param _token address The address of the token
-      * @param _spender address The address which will spend the funds.
-      * @param _subtractedValue uint256 The amount of tokens to decrease the allowance by.
-      * @param _fee uint256 The amount of tokens paid to msg.sender, by the owner.
-      * @param _nonce uint256 Presigned transaction number.
-      */
-    function decreaseApprovalPreSignedHashing(
+
+    /**
+    * @notice Hash (keccak256) of the payload used by decreaseApprovalPreSigned
+    * @param _token address The address of the token
+    * @param _spender address The address which will spend the funds.
+    * @param _subtractedValue uint256 The amount of tokens to decrease the allowance by.
+    * @param _fee uint256 The amount of tokens paid to msg.sender, by the owner.
+    * @param _nonce uint256 Presigned transaction number.
+    */
+    function decreaseApprovalPreSignedHashing
+    (
         address _token,
         address _spender,
         uint256 _subtractedValue,
         uint256 _fee,
         uint256 _nonce
     )
-        public
-        pure
-        returns (bytes32)
+      public
+      pure
+      returns (bytes32)
     {
-        /* "59388d78": decreaseApprovalPreSignedHashing(address,address,uint256,uint256,uint256) */
+      /* "59388d78": decreaseApprovalPreSignedHashing(address,address,uint256,uint256,uint256) */
         return keccak256(abi.encodePacked(bytes4(0x59388d78), _token, _spender, _subtractedValue, _fee, _nonce));
     }
 
+    
     /**
-     * @notice Hash (keccak256) of the payload used by transferFromPreSigned
-     * @param _token address The address of the token
-     * @param _from address The address which you want to send tokens from.
-     * @param _to address The address which you want to transfer to.
-     * @param _value uint256 The amount of tokens to be transferred.
-     * @param _fee uint256 The amount of tokens paid to msg.sender, by the spender.
-     * @param _nonce uint256 Presigned transaction number.
-     */
-    function transferFromPreSignedHashing(
+    * @notice Hash (keccak256) of the payload used by transferFromPreSigned
+    * @param _token address The address of the token
+    * @param _from address The address which you want to send tokens from.
+    * @param _to address The address which you want to transfer to.
+    * @param _value uint256 The amount of tokens to be transferred.
+    * @param _fee uint256 The amount of tokens paid to msg.sender, by the spender.
+    * @param _nonce uint256 Presigned transaction number.
+    */
+    function transferFromPreSignedHashing
+    (
         address _token,
         address _from,
         address _to,
@@ -351,57 +364,56 @@ contract ERC865Token is ERC865, PausableToken, MintableToken {
         uint256 _fee,
         uint256 _nonce
     )
-        public
-        pure
-        returns (bytes32)
+      public
+      pure
+      returns (bytes32)
     {
-        /* "b7656dc5": transferFromPreSignedHashing(address,address,address,uint256,uint256,uint256) */
+      /* "b7656dc5": transferFromPreSignedHashing(address,address,address,uint256,uint256,uint256) */
         return keccak256(abi.encodePacked(bytes4(0xb7656dc5), _token, _from, _to, _value, _fee, _nonce));
     }
 
+
     /**
-     * @notice Recover signer address from a message by using his signature
-     * @param hash bytes32 message, the hash is the signed message. What is recovered is the signer address.
-     * @param sig bytes signature, the signature is generated using web3.eth.sign()
-     */
-    function recover(bytes32 hash, bytes sig) public pure returns (address) {
-      bytes32 r;
-      bytes32 s;
-      uint8 v;
+    * @notice Recover signer address from a message by using his signature
+    * @param hash bytes32 message, the hash is the signed message. What is recovered is the signer address.
+    * @param sig bytes signature, the signature is generated using web3.eth.sign()
+    */
+    function recover 
+    (
+        bytes32 hash, 
+        bytes sig
+    ) 
+      public 
+      pure 
+      returns (address) 
+    {
+        bytes32 r;
+        bytes32 s;
+        uint8 v;
 
       //Check the signature length
-      if (sig.length != 65) {
-        return (address(0));
-      }
+        if (sig.length != 65) {
+            return (address(0));
+        }
 
       // Divide the signature in r, s and v variables
-      assembly {
-        r := mload(add(sig, 32))
-        s := mload(add(sig, 64))
-        v := byte(0, mload(add(sig, 96)))
-      }
+        assembly {
+          r := mload(add(sig, 32))
+          s := mload(add(sig, 64))
+          v := byte(0, mload(add(sig, 96)))
+        }
 
       // Version of signature should be 27 or 28, but 0 and 1 are also possible versions
-      if (v < 27) {
-        v += 27;
-      }
+        if (v < 27) {
+            v += 27;
+        }
 
       // If the version is correct return the signer address
-      if (v != 27 && v != 28) {
-        return (address(0));
-      } else {
-        return ecrecover(hash, v, r, s);
-      }
+        if (v != 27 && v != 28) {
+            return (address(0));
+        } else {
+            return ecrecover(hash, v, r, s);
+        }
     }
-       
- /*   function recoverAddr(bytes32 msgHash, uint8 v, bytes32 r, bytes32 s) public pure returns (address) {
-        return ecrecover(msgHash, v, r, s);
-    }
-
-    function isSigned(address _addr, bytes32 msgHash, uint8 v, bytes32 r, bytes32 s) public pure returns (bool) {
-        return ecrecover(msgHash, v, r, s) == _addr;
-    }*/
-      
-    
 
 }
